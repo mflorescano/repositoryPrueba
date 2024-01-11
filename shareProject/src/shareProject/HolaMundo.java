8168e2798e405cc0a458cc7c52ba9d222a978281
mflorescano <118437403+mflorescano@users.noreply.github.com>
2024-01-11 11:50:11 +0100

    mi cuarto commit

--- shareProject/src/shareProject/HolaMundo.java
+++ shareProject/src/shareProject/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 System.out.println("hola mundo");
-		System.out.println("adios mundo");
+		System.out.println("adios mundo cruel");
 	}
 
 }
