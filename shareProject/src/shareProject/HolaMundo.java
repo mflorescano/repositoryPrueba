47587c79b01d3fc30525a761ac9314fa218100d6
mflorescano <118437403+mflorescano@users.noreply.github.com>
2024-01-11 11:57:17 +0100

    Update HolaMundo.java
    commit desde remoto

--- shareProject/src/shareProject/HolaMundo.java
+++ shareProject/src/shareProject/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 System.out.println("hola mundo");
 		System.out.println("adios mundo cruel");
+		System.out.println("adios pringaos");
 	}
 
 }
